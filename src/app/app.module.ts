6db91fd49e2f919f7449a91b9ca79ecc2f950fa1
Edenn <deozza@gmail.com>
2019-08-21 14:17:07 +0200

    more cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule }                         from '@angular/platform-browser';
+import { NgModule }                              from '@angular/core';
+
+import { AppRoutingModule }                      from './app-routing.module';
+import { AppComponent }                          from './app.component';
+import {FormsModule}                             from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule}     from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
-
+    AppComponent
   ],
   imports: [
-    HttpClientModule,
-    BrowserAnimationsModule,
-    FormsModule,
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
 
   providers: [
