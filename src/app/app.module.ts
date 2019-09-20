bdbaddb7ed6667340335dad28796edd17e2453d8
A.MEGEVANT <antoine.megevant@gmail.com>
2019-09-20 12:17:33 +0200

    PWA  ng add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,8 @@ import { ContactComponent } from './ui/contact/contact.component';
 import { FaqComponent } from './ui/faq/faq.component';
 import { ConditionsPrivacyComponent } from './ui/conditions-privacy/conditions-privacy.component';
 import { FourOhFourComponent } from './ui/four-oh-four/four-oh-four.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -74,6 +76,7 @@ import { FourOhFourComponent } from './ui/four-oh-four/four-oh-four.component';
     ReactiveFormsModule,
     FileUploadModule,
     MatIconModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
 
 
   ],
