26fe441def234d17b4aa379e12f2644b7d1c6533
Aminata <ndiayebouchraa@gmail.com>
2019-09-22 00:04:16 +0200

    fix profil-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
@@ -82,11 +83,9 @@ import { MessengerComponent } from './ui/messenger/messenger.component';
 
   providers: [
     {provide: HTTP_INTERCEPTORS,
-    useClass: AuthTokenInterceptor,
-
-
-    multi: true,
+    useClass: AuthTokenInterceptor, multi: true,
   }
+  
 
     ],
   bootstrap: [AppComponent]
