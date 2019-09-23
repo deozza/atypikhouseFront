d65b34218201ddf14fca44451e38c1553c32d8fd
Aïcha <yomikoda@gmail.com>
2019-09-23 13:57:10 +0200

    BEG NAV CORRECTIONS

--- src/app/ui/admin/admin-nav/admin-nav.component.ts
+++ src/app/ui/admin/admin-nav/admin-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-admin-nav',
@@ -10,6 +11,14 @@ export class AdminNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      $('#admin-nav ul').css('top','500px');
+      let scroll = $('body').scrollTop();
+      console.log("SCROLL"+scroll);
+      $('.question-answer').click(function() {
+        $(this).toggleClass( 'questionOpen');
+      });
+  });
   }
 
 }
